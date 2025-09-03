226c5d43f20fec10f4236c8da7c2f5c197153e47
Code5mith <jacobscontact1@gmail.com>
2025-09-03 13:49:25 -0700

    Refactor : update query script

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,7 +4,7 @@ SELECT * FROM Booking b INNER JOIN User u ON b.user_id = u.user_id;
 
 --Write a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.--
 
-SELECT * FROM Property p LEFT JOIN Review r ON p.property_id = r.property_id;
+SELECT * FROM Property p LEFT JOIN Review r ON p.property_id = r.property_id ORDER BY p.property_id;
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user. --
 
