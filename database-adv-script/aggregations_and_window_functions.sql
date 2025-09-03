391be49d1e593276d814438b33a8a3f441ba3ac1
Code5mith <jacobscontact1@gmail.com>
2025-09-03 16:25:15 -0700

    Task : aggregation and window function

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,18 @@
+-- Aggregation
+SELECT user_id, COUNT(*) AS total_bookings
+	FROM Booking
+	GROUP BY user_id
+	ORDER BY total_bookings DESC;
+
+
+-- window function 
+WITH PropertyBookingCount AS (
+    SELECT property_id, COUNT(booking_id) AS booking_count
+    FROM Booking
+    GROUP BY property_id
+) SELECT -- query must be in the same line 
+    property_id,
+    booking_count,
+    RANK() OVER (ORDER BY booking_count DESC) AS property_rank
+FROM PropertyBookingCount
+ORDER BY proper ty_rank ASC;
