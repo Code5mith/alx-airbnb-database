6a0834dea6cb407bd1ee6e63b14f002ba0313297
Code5mith <jacobscontact1@gmail.com>
2025-09-03 16:51:28 -0700

    fix : add row_number window funcion

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,12 +7,19 @@ SELECT user_id, COUNT(*) AS total_bookings
 
 -- window function 
 WITH PropertyBookingCount AS (
-    SELECT property_id, COUNT(booking_id) AS booking_count
-    FROM Booking
-    GROUP BY property_id
-) SELECT -- query must be in the same line 
+    SELECT
+        property_id,
+        COUNT(booking_id) AS booking_count
+    FROM
+        Booking
+    GROUP BY
+        property_id
+) SELECT
     property_id,
     booking_count,
-    RANK() OVER (ORDER BY booking_count DESC) AS property_rank
-FROM PropertyBookingCount
-ORDER BY property_rank ASC;
+    RANK() OVER (ORDER BY booking_count DESC) AS property_rank_rank,
+    ROW_NUMBER() OVER (ORDER BY booking_count DESC) AS property_rank_row_number
+FROM
+    PropertyBookingCount
+ORDER BY
+    booking_count DESC;
