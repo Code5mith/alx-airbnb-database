dde4a51907c96d91ebd5c07b9f45968fe502d2c1
Code5mith <jacobscontact1@gmail.com>
2025-09-03 17:54:50 -0700

    Fix : DB index script

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,6 +1,10 @@
 -- Create indexes on primary key and foreign key columns for faster joins and lookups.
 
 -- Index for the User table's primary key
+
+-- Before indexing 
+EXPLAIN SELECT * FROM "User" u WHERE u."role" == "guest"; ANALYZE
+
 CREATE INDEX idx_user_id ON User (user_id);
 
 -- Index for the Booking table's user_id foreign key
@@ -18,5 +22,5 @@ CREATE INDEX idx_review_property_id ON Review (property_id);
 -- Add an index on a frequently searched column like the 'location' in the Property table.
 CREATE INDEX idx_property_location ON Property (location);
 
-
+-- After indexing 
 EXPLAIN SELECT * FROM "User" u WHERE u."role" == "guest"; ANALYZE
